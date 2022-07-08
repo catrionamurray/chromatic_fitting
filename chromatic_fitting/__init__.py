de17bac5101deab14335ac83449c9a021a48a272
Catriona Murray <cam217@cam.ac.uk>
2022-07-08 11:59:30 -0600

    moved parameters.py

--- chromatic_fitting/__init__.py
+++ chromatic_fitting/__init__.py
@@ -1,6 +1,7 @@
 from .version import *
 from .imports import *
 from .chromatic_fitting import *
+from .parameters import *
 from .inject_spectrum import *
 from .recover_transit import *
 from .spectrum import *
