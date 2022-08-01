e47a7284a355c4e9bbc56d69dc44cc116b70b0db
Catriona Murray <34891147+catrionamurray@users.noreply.github.com>
2022-08-01 15:30:35 -0600

    more JWST tests

---
+++ build/lib/chromatic_fitting/imports.py
@@ -0,0 +1,20 @@
+import chromatic
+import numpy as np
+
+import exoplanet as xo
+
+# import starry
+# import theano
+# import pickle5 as pickle # needed for Python v3.7!
+import sys
+from chromatic import *
+
+import pymc3 as pm
+import pymc3_ext as pmx
+
+print(f"Running on Python v{sys.version}")
+print(f"Running on numpy v{np.__version__}")
+print(f"Running on chromatic v{chromatic.version()}")
+# print("chromatic version = " + str(chromatic.version()))
+# print("numpy version = " + str(np.__version__))
+# print("theano version = " + str(theano.__version__))
